d8b0fb402eb53abbd452cd6dc9df986a75e7f399
Thomas Roberts <thomas@edgethreesixty.com>
2016-07-29 08:52:58 +0100

    changed artist descriptions and stuff

--- resources/views/components/cover-photo.blade.php
+++ resources/views/components/cover-photo.blade.php
@@ -1,4 +1,4 @@
-<div class="cover-photo" style="background-image: url('https://unsplash.it/1920/207')">
+<div class="cover-photo" style="background-image: url('https://unsplash.it/1920/201')">
     <div class="cover-photo__artist-name">
         <div class="container">
             <div class="cover-photo__artist-name__inner">
